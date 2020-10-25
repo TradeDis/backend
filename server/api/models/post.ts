d5b00ae32b3e362263336bcfa269e4653667205f
Ryan How <ryanhow1999@yahoo.ca>
2020-10-25 00:32:09 -0400

    Post model added

--- server/api/models/post.ts
+++ server/api/models/post.ts
@@ -4,16 +4,67 @@ import sequence from "mongoose-sequence";
 const AutoIncrement = sequence(mongoose);
 
 export interface IPostModel extends mongoose.Document {
-  // TODO
+  post_id: number;
+  title: string;
+  type: string;
+  content: string;
+  created_by: {
+    user_id: number;
+    username: string;
+    full_name: string;
+    avatar: string;
+  };
+  date: Date;
+  status: string;
+  tags: string[];
+  comments: [
+    {
+      comment_id: number;
+      created_by: {
+        user_id: number;
+        username: string;
+        full_name: string;
+        avatar: string;
+      };
+      comment: string;
+    }
+  ];
 }
+
+const poster = new mongoose.Schema({
+  user_id: Number,
+  username: String,
+  full_name: String,
+  avatar: String
+});
+
+const commenter = new mongoose.Schema({
+  user_id: Number,
+  username: String,
+  full_name: String,
+  avatar: String
+});
+
+const comment = new mongoose.Schema({
+  comment_id: String,
+  created_by: commenter,
+  comment: String
+});
 
 const posts = new mongoose.Schema(
   {
     post_id: { type: Number, unique: true },
-    // TODO
+    title: String,
+    type: String,
+    content: String,
+    created_by: poster,
+    date: Date,
+    status: String,
+    tags: [String],
+    comments: [comment]
   },
   {
-    collection: "posts",
+    collection: "posts"
   }
 );
 
